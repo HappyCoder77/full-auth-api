154dbd795c3f7c388825be44f7b5560d975a22b9
HappyCoder77 <saul_rr77@hotmail.com>
2024-06-07 06:50:34 -0400

    intial commit

---
+++ users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path, re_path
+from .views import (
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
+    LogoutView,
+    CustomProviderAuthView
+)
+
+urlpatterns = [
+    re_path(
+        r'^o/(?P<provider>\S+)/$',
+        CustomProviderAuthView.as_view(),
+        name='provider-auth'
+    ),
+    path('jwt/create/', CustomTokenObtainPairView.as_view(), name='jwt_create'),
+    path('jwt/refresh/', CustomTokenRefreshView.as_view(), name='jwt_refresh'),
+    path('jwt/verify/', CustomTokenVerifyView.as_view(), name='jwt_verify'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
