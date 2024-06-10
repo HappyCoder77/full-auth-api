eb41f25377b958520b962f0d118c36601aeaf261
HappyCoder77 <saul_rr77@hotmail.com>
2024-06-10 11:55:54 -0400

    add custom media location

--- full_auth/settings.py
+++ full_auth/settings.py
@@ -166,11 +166,12 @@ else:
     }
     AWS_DEFAULT_ACL = 'public-read'
     AWS_LOCATION = 'static'
-    AWS_S3_CUSTOM_DOMAIN=getenv('AWS_S3_CUSTOM_DOMAIN')
+    AWS_MEDIA_LOCATION = 'media'
+    AWS_S3_CUSTOM_DOMAIN = getenv('AWS_S3_CUSTOM_DOMAIN')
     
     STORAGES = {
         'default': {
-            'BACKEND': 'storages.backends.s3.S3Storage'
+            'BACKEND': 'custom_storages.CustomS3Storage'
         },
         'staticfiles': {
             'BACKEND': 'storages.backends.s3boto3.S3Boto3Storage'
---
+++ users/custom_storages.py
@@ -0,0 +1,5 @@
+from django.conf import settings
+from storages.backends.s3 import S3Storage
+
+class CustomS3Storage(S3Storage):
+    location = settings.AWS_MEDIA_LOCATION
