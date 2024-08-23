74d6da41fabee2ea7b413a38536cf55bd8559282
HappyCoder77 <saul_rr77@hotmail.com>
2024-08-23 07:01:36 -0400

    override password reset email template

--- users/email.py
+++ users/email.py
@@ -3,3 +3,7 @@ from djoser import email
 
 class ActivationEmail(email.ActivationEmail):
     template_name = 'users/activation.html'
+
+
+class PasswordResetEmail(email.PasswordResetEmail):
+    template_name = 'users/password_reset.html'
