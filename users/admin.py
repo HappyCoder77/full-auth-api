208e018f14f73e379e68b75676ae377f7ab2f47f
HappyCoder77 <saul_rr77@hotmail.com>
2024-09-24 10:15:28 -0400

    add BaseProfile and RegionalManager Models

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
-from .models import UserAccount
+from .models import UserAccount, RegionalManager
+
+
+class RegionalManagerAdmin(admin.ModelAdmin):
+    fields = ['email', 'first_name', 'middle_name', 'first_last_name',
+              'second_last_name', 'sex', 'birthdate', 'created_by']
+
 
 admin.site.register(UserAccount)
+admin.site.register(RegionalManager, RegionalManagerAdmin)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.utils.translation import gettext_lazy as _
+
+
+GENERO_CHOICES = [
+    ("M", "Masculino"),
+    ("F", "Femenino")
+]
 
 
 class UserAccountManager(BaseUserManager):
@@ -51,3 +59,28 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+# TODO: add Address model
+
+
+class BaseProfile(models.Model):
+    # TODO: add address field
+    # TODO: añadir validacion a la fecha de nacimiento para que usuarios distintos de coleccionistas deban ser mayores de edad
+    first_name = models.CharField(max_length=50)
+    middle_name = models.CharField(max_length=50, null=True, blank=True)
+    first_last_name = models.CharField(max_length=50)
+    second_last_name = models.CharField(max_length=50, null=True, blank=True)
+    sex = models.CharField(max_length=10, choices=GENERO_CHOICES)
+    birthdate = models.DateField(null=True, blank=True)
+
+
+class RegionalManager(BaseProfile):
+    user = models.OneToOneField(
+        UserAccount, on_delete=models.CASCADE, null=True)
+    email = models.EmailField(_("Email field"), unique=True)
+    created_by = models.ForeignKey(
+        UserAccount, on_delete=models.SET_NULL, null=True, related_name='created_profiles')
+
+    def __str__(self) -> str:
+        return self.first_name + " " + self.first_last_name
