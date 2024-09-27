9ed0bc769f9c352b0333e6eefb26a7bc8a2c384f
HappyCoder77 <saul_rr77@hotmail.com>
2024-09-26 21:05:21 -0400

    fix typos in admin and serializer

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from .models import UserAccount, RegionalManager
 
 class RegionalManagerAdmin(admin.ModelAdmin):
     fields = ['email', 'first_name', 'middle_name', 'first_last_name',
-              'second_last_name', 'sex', 'birthdate', 'created_by']
+              'second_last_name', 'gender', 'birthdate', 'created_by']
 
 
 admin.site.register(UserAccount)
--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ class RegionalManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = RegionalManager
         fields = ['id', 'user', 'first_name', 'middle_name', 'first_last_name',
-                  'second_last_name', 'sex', 'birthdate', 'email', 'created_by']
+                  'second_last_name', 'gender', 'birthdate', 'email', 'created_by']
 
         extra_kwargs = {
             "user": {"write_only": True}
