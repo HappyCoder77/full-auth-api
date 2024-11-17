eae9557869bc9ae4f3edec1a1cefccd8a836d603
HappyCoder77 <saul_rr77@hotmail.com>
2024-11-17 11:07:26 -0400

    fix bug in urls

--- full_auth/urls.py
+++ full_auth/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework.documentation import include_docs_urls
 from rest_framework.schemas import get_schema_view
@@ -26,3 +28,6 @@ urlpatterns = [
     path('docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
     path('schema/', schema_view)
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
