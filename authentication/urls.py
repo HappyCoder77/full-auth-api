24a1483cd5eacd09b030141be493f11a5c96abc6
HappyCoder77 <saul_rr77@hotmail.com>
2024-12-05 12:04:30 -0400

    add password validation messages view

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
-from .views import (CustomTokenObtainPairView,
-                    CustomTokenRefreshView, CustomTokenVerifyView, LogoutView)
+from .views import (
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
+    LogoutView,
+    password_help,
+)
 
 urlpatterns = [
-    path('', include('djoser.urls')),
-    path('jwt/create/', CustomTokenObtainPairView.as_view(), name='jwt_create'),
-    path('jwt/refresh/', CustomTokenRefreshView.as_view(), name='jwt_refresh'),
-    path('jwt/verify/', CustomTokenVerifyView.as_view(), name='jwt_verify'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path("", include("djoser.urls")),
+    path("password-help/", password_help, name="password-help"),
+    path("jwt/create/", CustomTokenObtainPairView.as_view(), name="jwt_create"),
+    path("jwt/refresh/", CustomTokenRefreshView.as_view(), name="jwt_refresh"),
+    path("jwt/verify/", CustomTokenVerifyView.as_view(), name="jwt_verify"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,12 @@
 from django.conf import settings
+from django.contrib.auth.password_validation import (
+    UserAttributeSimilarityValidator,
+    MinimumLengthValidator,
+    CommonPasswordValidator,
+    NumericPasswordValidator,
+)
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,7 +14,7 @@ from rest_framework import status
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
-    TokenVerifyView
+    TokenVerifyView,
 )
 
 
@@ -15,27 +23,27 @@ class CustomTokenObtainPairView(TokenObtainPairView):
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            access_token = response.data.get('access')
-            refresh_token = response.data.get('refresh')
+            access_token = response.data.get("access")
+            refresh_token = response.data.get("refresh")
 
             response.set_cookie(
-                'access',
+                "access",
                 access_token,
                 max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE
+                samesite=settings.AUTH_COOKIE_SAMESITE,
             )
 
             response.set_cookie(
-                'refresh',
+                "refresh",
                 refresh_token,
                 max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE
+                samesite=settings.AUTH_COOKIE_SAMESITE,
             )
 
         return response
@@ -43,24 +51,24 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 class CustomTokenRefreshView(TokenRefreshView):
     def post(self, request, *args, **kwargs):
-        refresh_token = request.COOKIES.get('refresh')
+        refresh_token = request.COOKIES.get("refresh")
 
         if refresh_token:
-            request.data['refresh'] = refresh_token
+            request.data["refresh"] = refresh_token
 
         response = super().post(request, *args, **kwargs)
 
         if response.status_code == 200:
-            access_token = response.data.get('access')
+            access_token = response.data.get("access")
 
             response.set_cookie(
-                'access',
+                "access",
                 access_token,
                 max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
                 path=settings.AUTH_COOKIE_PATH,
                 secure=settings.AUTH_COOKIE_SECURE,
                 httponly=settings.AUTH_COOKIE_HTTP_ONLY,
-                samesite=settings.AUTH_COOKIE_SAMESITE
+                samesite=settings.AUTH_COOKIE_SAMESITE,
             )
 
         return response
@@ -68,10 +76,10 @@ class CustomTokenRefreshView(TokenRefreshView):
 
 class CustomTokenVerifyView(TokenVerifyView):
     def post(self, request, *args, **kwargs):
-        access_token = request.COOKIES.get('access')
+        access_token = request.COOKIES.get("access")
 
         if access_token:
-            request.data['token'] = access_token
+            request.data["token"] = access_token
 
         return super().post(request, *args, **kwargs)
 
@@ -79,7 +87,21 @@ class CustomTokenVerifyView(TokenVerifyView):
 class LogoutView(APIView):
     def post(self, request, *args, **kwargs):
         response = Response(status=status.HTTP_204_NO_CONTENT)
-        response.delete_cookie('access')
-        response.delete_cookie('refresh')
+        response.delete_cookie("access")
+        response.delete_cookie("refresh")
 
         return response
+
+
+@api_view(["GET"])
+@permission_classes([AllowAny])
+def password_help(request):
+    validators = [
+        UserAttributeSimilarityValidator(),
+        MinimumLengthValidator(),
+        CommonPasswordValidator(),
+        NumericPasswordValidator(),
+    ]
+
+    help_texts = [validator.get_help_text() for validator in validators]
+    return Response(help_texts)
