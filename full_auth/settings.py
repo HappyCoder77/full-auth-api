a236a4d5887e239311b5529d5282c98cf1d33f10
HappyCoder77 <saul_rr77@hotmail.com>
2025-02-12 18:41:28 -0400

    update settings file for production

--- full_auth/settings.py
+++ full_auth/settings.py
@@ -257,7 +257,7 @@ DJOSER = {
     "USER_CREATE_PASSWORD_RETYPE": True,
     "PASSWORD_RESET_CONFIRM_RETYPE": True,
     "TOKEN_MODEL": None,
-    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": getenv("REDIRECT_URLS").split(","),
+    # "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": getenv("REDIRECT_URLS").split(","),
     "PASSWORD_VALIDATORS": [password_validation.validate_password],
 }
 
