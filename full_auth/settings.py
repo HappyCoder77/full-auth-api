18a3119071fffcea3e53d9ff1f8fad0b7a2d35df
HappyCoder77 <saul_rr77@hotmail.com>
2025-02-13 16:21:30 -0400

    fix storages config

--- full_auth/settings.py
+++ full_auth/settings.py
@@ -223,10 +223,16 @@ else:
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_S3_FILE_OVERWRITE = False
 
-    STORAGES = {
-        "default": {"BACKEND": "custom_storages.CustomS3Storage"},
-        "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
-    }
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {"location": "media"},
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS": {"location": "static"},
+    },
+}
 
 
 AUTHENTICATION_BACKENDS = [
